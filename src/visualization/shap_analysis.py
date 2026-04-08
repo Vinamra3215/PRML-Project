d70581e2d840217afee4f89e5b7a02e80041182d
dc1306 <naughtydc5039@gmail.com>
2026-04-08 13:40:55 +0530

    feat(viz): add SHAP analysis visualization

---
+++ src/visualization/shap_analysis.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def shap_feature_importance(model, X_train, X_test, feature_names=None, save_path=None):
+    try:
+        import shap
+        explainer = shap.KernelExplainer(model.predict_proba, shap.sample(X_train, 100))
+        shap_values = explainer.shap_values(X_test[:50])
+        plt.figure(figsize=(12, 8))
+        shap.summary_plot(shap_values, X_test[:50], feature_names=feature_names, show=False)
+        if save_path:
+            plt.savefig(save_path, dpi=150, bbox_inches="tight")
+        plt.close()
+    except ImportError:
+        print("Install shap: pip install shap")
